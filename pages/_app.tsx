049d6b90608859d43143d24dfc58bac5600acb0d
Felipe Oucharski <felipe.oucharski@andritz.com>
2023-03-13 16:05:45 +0100

    Use SplashScreen, Theme, Authentication & LayoutProvider on _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import { AuthenticationProvider } from "@/providers";
+import { LayoutProvider } from "@/components";
+import { useHideSplashScreen } from "@/components/SplashScreen";
+import { settings } from "settings";
+
+const { layout, theme } = settings;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useHideSplashScreen();
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthenticationProvider>
+        <LayoutProvider {...layout}>
+          <Component {...pageProps} />
+        </LayoutProvider>
+      </AuthenticationProvider>
+    </ThemeProvider>
+  );
 }
